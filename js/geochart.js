02c79a86816ffa003e8f2ed6256949cea8fac8fc
Rodrigo Erazo Hermosilla <rodrigoieh@assembly.cl>
2021-04-15 13:28:16 -0400

    apikey

--- js/geochart.js
+++ js/geochart.js
@@ -11,23 +11,23 @@ google.charts.load('current', {
 google.charts.setOnLoadCallback(drawRegionsMap);
 
 function drawRegionsMap() {
-    // TODO: debug country_count_group_by_continent
-    // from data/country.js {code_count, country_count, country_count_group_by_continent, country_count_clean}
-    var data_country_count = google.visualization.arrayToDataTable(country_count);
-    var data_code_count = google.visualization.arrayToDataTable(code_count);
+	// TODO: debug country_count_group_by_continent
+	// from data/country.js {code_count, country_count, country_count_group_by_continent, country_count_clean}
+	var data_country_count = google.visualization.arrayToDataTable(country_count);
+	var data_code_count = google.visualization.arrayToDataTable(code_count);
 
-    var options = {
-        // dataMode: 'regions',
-        legend: 'none',
-        // displayMode: 'text'
-        colorAxis: {
-            colors: ['darkblue']
-        }
-    };
+	var options = {
+		// dataMode: 'regions',
+		legend: 'none',
+		// displayMode: 'text'
+		colorAxis: {
+			colors: ['darkblue']
+		}
+	};
 
-    var chart_country_count = new google.visualization.GeoChart(document.getElementById('div_geochart_country_count'));
-    var chart_code_count = new google.visualization.GeoChart(document.getElementById('div_geochart_code_count'));
+	var chart_country_count = new google.visualization.GeoChart(document.getElementById('div_geochart_country_count'));
+	var chart_code_count = new google.visualization.GeoChart(document.getElementById('div_geochart_code_count'));
 
-    chart_country_count.draw(data_country_count, options);
-//    chart_code_count.draw(data_code_count, options);
+	chart_country_count.draw(data_country_count, options);
+	// chart_code_count.draw(data_code_count, options);
 };
