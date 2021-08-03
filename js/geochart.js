568fc64de849487bba38858e0e88a7194a1823f3
Rodrigo Erazo Hermosilla <rodrigoieh@assembly.cl>
2021-08-03 10:20:59 -0400

    viewport

--- js/geochart.js
+++ js/geochart.js
@@ -5,7 +5,21 @@
     });
 
     google.charts.setOnLoadCallback(drawRegionsMap);
+
+    setViewportMetadata();
 }());
+
+function setViewportMetadata() {
+    if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+        let viewport = document.querySelector('meta[name="viewport"]');
+        if (viewport) {
+            viewport.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+            document.body.addEventListener('gesturestart', () => {
+                viewport.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+            }, false);
+        }
+    }
+}
 
 function drawRegionsMap() {
     const countriesDataTable = google.visualization.arrayToDataTable(countries);
@@ -19,4 +33,3 @@ function drawRegionsMap() {
     const geochart = new google.visualization.GeoChart(geochartDiv);
     geochart.draw(countriesDataTable, options);
 }
-
