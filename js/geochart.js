f71cd5e4f0d83fb3afdcddd9fee61b33ce134c17
Rodrigo Erazo Hermosilla <rodrigoieh@assembly.cl>
2021-08-03 10:13:45 -0400

    viewport

--- js/geochart.js
+++ js/geochart.js
@@ -1,9 +1,21 @@
-google.charts.load('current', {
-    'packages': ['geochart'],
-    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-});
+(function () {
+    google.charts.load('current', {
+        'packages': ['geochart'],
+        'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+    });
 
-google.charts.setOnLoadCallback(drawRegionsMap);
+    google.charts.setOnLoadCallback(drawRegionsMap);
+
+    if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
+        let viewportmeta = document.querySelector('meta[name="viewport"]');
+        if (viewportmeta) {
+            viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
+            document.body.addEventListener('gesturestart', () => {
+                viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
+            }, false);
+        }
+    }
+}());
 
 function drawRegionsMap() {
     const countriesDataTable = google.visualization.arrayToDataTable(countries);
@@ -17,3 +29,4 @@ function drawRegionsMap() {
     const geochart = new google.visualization.GeoChart(geochartDiv);
     geochart.draw(countriesDataTable, options);
 }
+
